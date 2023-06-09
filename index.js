139dbd1929a26ccba20967d46517c500afe9de8c
Afra <propazaman12@gmail.com>
2023-06-09 20:17:25 +0600

    user patch added

--- index.js
+++ index.js
@@ -33,10 +33,37 @@ async function run() {
     const userCollection = client.db("bistroDb").collection("users");
 
     // user related api
+    app.get('/users', async(req, res) =>{
+      const result = await userCollection.find().toArray();
+      res.send(result);
+  })
+
     app.post('/users', async (req, res) => {
       const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: 'user already exists' })
+      }
+
       const result = await userCollection.insertOne(user);
       res.send(result);
+    })
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
     })
 
     // menu related apis
