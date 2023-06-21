02eb7a0f301b4e7c005d8a3c501c658fb673b6c1
Afra <propazaman12@gmail.com>
2023-06-21 13:32:23 +0600

    menu delete

--- index.js
+++ index.js
@@ -137,6 +137,13 @@ async function run() {
       const result = await menuCollection.insertOne(newItem)
       res.send(result);
     })
+
+    app.delete('/menu/:id', verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    })
     
     // review related apis
     app.get('/reviews', async(req, res) =>{
