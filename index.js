f30687832060dbfbaf0639bc8abd85411470e48c
Afra <propazaman12@gmail.com>
2023-06-21 13:28:18 +0600

    menu post

--- index.js
+++ index.js
@@ -130,6 +130,13 @@ async function run() {
         const result = await menuCollection.find().toArray();
         res.send(result);
     })
+
+
+    app.post('/menu', verifyJWT, verifyAdmin, async (req, res) => {
+      const newItem = req.body;
+      const result = await menuCollection.insertOne(newItem)
+      res.send(result);
+    })
     
     // review related apis
     app.get('/reviews', async(req, res) =>{
